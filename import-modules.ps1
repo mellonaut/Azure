5aa6cbe3ae17746ba79cbb208a1a15c52ba9715a
mellonaut <65114647+mellonaut@users.noreply.github.com>
2022-07-02 12:39:47 -0400

    Modules
    Import and Install scripts

---
+++ import-modules.ps1
@@ -0,0 +1,6 @@
+Import-Module Az 
+Import-Module AzureAD 
+Import-Module Microsoft.Graph  
+Import-Module MSOnline         
+Import-Module AzureADPreview   
+Import-Module AADInternals 
---
+++ install-modules.ps1
@@ -0,0 +1,6 @@
+Install-Module Az -Force -Confirm -AllowClobber -Scope CurrentUser
+Install-Module AzureAD -Force -Confirm -AllowClobber -Scope CurrentUser
+Install-Module Microsoft.Graph -Force -Confirm -AllowClobber -Scope CurrentUser 
+Install-Module MSOnline -Force -Confirm -AllowClobber -Scope CurrentUser        
+Install-Module AzureADPreview -Force -Confirm -AllowClobber -Scope CurrentUser  
+Install-Module AADInternals -Force -Confirm -AllowClobber -Scope CurrentUser 
